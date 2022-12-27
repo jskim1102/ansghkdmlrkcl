4f065dec5c4a118294203c2d087d539e207261fb
jskim1102 <96895094+jskim1102@users.noreply.github.com>
2022-12-27 12:58:56 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import setup, find_packages
 
+def imshow(img):
+    cv2.imshow('image', img)
+    cv2.waitKey() 
+    cv2.destroyAllWindows()
+
 setup(
     name                = 'ansghkdmlrkcl',
     version             = '0.1',
@@ -11,7 +16,9 @@ setup(
     install_requires    =  [],
     packages            = find_packages(exclude = []),
     keywords            = ['ansghkdmlrkcl'],
-    python_requires     = '>=3',
+    python_requires     = '>=3.8',
+    install_requires=[
+        "opencv-python"
     package_data        = {},
     zip_safe            = False,
     classifiers         = [
