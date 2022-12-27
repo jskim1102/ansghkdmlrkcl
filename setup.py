c341b0384195712cb306142a8673f7b732d67683
jskim1102 <96895094+jskim1102@users.noreply.github.com>
2022-12-27 11:09:47 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name                = 'ansghkdmlrkcl',
+    version             = '0.1',
+    description         = 'ansghkdmlrkcl',
+    author              = 'jinsoo',
+    author_email        = 'deepi.contact.us@gmail.com',
+    url                 = 'https://github.com/jeakwon/ansghkdmlrkcl',
+    download_url        = 'https://github.com/jeakwon/ansghkdmlrkcl/archive/0.0.tar.gz',
+    install_requires    =  [],
+    packages            = find_packages(exclude = []),
+    keywords            = ['ansghkdmlrkcl'],
+    python_requires     = '>=3',
+    package_data        = {},
+    zip_safe            = False,
+    classifiers         = [
+        'Programming Language :: Python :: 3.8.13',
+    ],
+)
